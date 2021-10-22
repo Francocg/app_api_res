bbfe2d18b6c40f312c846b5986a51054cb07f40f
francocg <francocabanillas@upeu.edu.pe>
2021-10-21 20:54:56 -0500

    vv

--- bin/app_api_res.dart
+++ bin/app_api_res.dart
@@ -5,9 +5,9 @@ import 'package:http/http.dart' as http;
 void main(List<String> arguments) {
   final url = Uri.parse('http://localhost:3000/api/listar');
   http.get(url).then((res) {
-    final usuarios = convert.jsonDecode(res.body);
-    for (int i = 0; i < usuarios.length; i++) {
-      print('${usuarios[i]['titulo']}');
+    final body = convert.jsonDecode(res.body);
+    for (int i = 0; i < 5; i++) {
+      print('${body['usuarios'][i]['titulo']}');
     }
   });
 }
